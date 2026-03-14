3f4c553e72659d626acbdee98f065f8efdaa1456
Abhishek2122-star <abhisheklad963@gmail.com>
2026-03-14 14:18:55 +0530

    Order Entity Created

---
+++ order-service/src/main/java/com/example/order/entity/Order.java
@@ -0,0 +1,33 @@
+package com.example.order.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;     // reference to User Service
+    private String product;
+    private int quantity;
+    private String status;
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getProduct() { return product; }
+    public void setProduct(String product) { this.product = product; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
